8e2afb89e9c82e5dc6db1c9c78429494ebf069b0
ZorroGuardaPavos <4ndres.or@gmail.com>
2025-02-08 23:19:48 -0800

    add client generation script and auto-generated client files

---
+++ scripts/generate_client.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+cd backend
+python -c "import src.main; import json; print(json.dumps(src.main.app.openapi()))" > ../openapi.json
+cd ..
+mv openapi.json frontend/
+cd frontend
+npx biome format --write openapi.json
+npm run generate-client
+npx biome format --write ./src/client
