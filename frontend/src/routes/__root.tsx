00b4e43075c9af42a6a3389db532b71179b503c7
ZorroGuardaPavos <4ndres.or@gmail.com>
2025-02-14 21:57:49 -0800

    feat: add version display to the root route for better user awareness

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { Text } from "@chakra-ui/react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 // import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
@@ -5,6 +6,9 @@ export const Route = createRootRoute({
 	component: () => (
 		<>
 			<Outlet />
+			<Text position="fixed" bottom={2} left={2} fontSize="xs" color="gray.500">
+				v0.0.1
+			</Text>
 			{/* <TanStackRouterDevtools /> */}
 		</>
 	),
