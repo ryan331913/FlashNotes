c4d6215baa758c377935cb00108ca460b238c68c
ZorroGuardaPavos <4ndres.or@gmail.com>
2025-02-24 16:23:01 +0100

    refactor: Remove local storage draft saving and simplify card saving workflow

--- frontend/src/components/cards/CardHeader.tsx
+++ frontend/src/components/cards/CardHeader.tsx
@@ -1,6 +1,6 @@
 import { HStack, IconButton, Text } from "@chakra-ui/react";
 import { FiRepeat } from "react-icons/fi";
-import { IoCheckmark, IoClose } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 
 interface CardHeaderProps {
 	side: "front" | "back";
@@ -44,7 +44,7 @@ function CardHeader({ side, onFlip, onSave }: CardHeaderProps) {
 					bg: "bg.50",
 				}}
 			>
-				{side === "back" ? <IoCheckmark size={20} /> : <IoClose />}
+				<IoClose />
 			</IconButton>
 		</HStack>
 	);
