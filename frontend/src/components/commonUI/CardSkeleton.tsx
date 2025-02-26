355bbce2769493c62fe5d13d2e4509775a84b394
ZorroGuardaPavos <4ndres.or@gmail.com>
2025-02-26 19:57:24 +0100

    feat: Create custom Skeleton components for enhanced loading UI

--- frontend/src/components/commonUI/CardSkeleton.tsx
+++ frontend/src/components/commonUI/CardSkeleton.tsx
@@ -1,10 +1,33 @@
-import { Container } from "@chakra-ui/react";
-import { Skeleton } from "@chakra-ui/react";
+import { Box, Flex, VStack } from "@chakra-ui/react";
+
+import { Skeleton, SkeletonText } from "@/components/ui/skeleton";
 
 export default function CardSkeleton() {
+	const skeletonCardStyles = {
+		padding: "1rem",
+		width: "100%",
+		height: "100%",
+		borderRadius: "lg",
+		borderWidth: "1px",
+		boxShadow: "sm",
+		borderColor: "bg.200",
+	};
+
 	return (
-		<Container width="100%" mt="2rem">
-			<Skeleton h="calc(100dvh - 12rem)" width="100%" bg="bg.100" />
-		</Container>
+		<VStack gap={4} h="calc(100dvh - 8rem)" width="100%">
+			<Flex height="4rem" w="100%" alignItems="end">
+				<SkeletonText noOfLines={2} gap="1" />
+			</Flex>
+
+			<Box position="relative" height="100%" width="100%" flex="1">
+				<Box {...skeletonCardStyles} bg="bg.50">
+					<Skeleton height="100%" width="100%" />
+				</Box>
+			</Box>
+
+			<Flex height="4rem" w="100%" justifyContent="center" alignItems="center">
+				<Skeleton height="50%" width="50%" />
+			</Flex>
+		</VStack>
 	);
 }
