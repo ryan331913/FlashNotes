5c13bc67427aece30c56c0d6a208c1c8118645f1
Jose Fernandez <jose.fernandez91@gmail.com>
2025-03-11 23:45:40 +0100

    install and setup of dependencies
    i18next: for internationalization
    i18next-browser-languagedetector: for detecting browser language set
    i18next-http-backend: for retrieving locales from node.js as backend or API
    react-i18next: plugin for using i18next within react

---
+++ frontend/src/i18n/index.tsx
@@ -0,0 +1,28 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+import { initReactI18next } from "react-i18next";
+
+export const LOCALE_KEYS = [
+	{ key: "en", name: "English", flag: "us" },
+	{ key: "es", name: "Español", flag: "es" },
+	{ key: "nl", name: "Nederlands", flag: "nl" },
+];
+
+export const i18nPromise = i18n
+	.use(Backend)
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		detection: {
+			order: ["localStorage", "navigator"],
+			lookupLocalStorage: "i18nextLng",
+			convertDetectedLanguage: (lng: string) => lng.replace("-", "_"),
+		},
+		supportedLngs: LOCALE_KEYS.map(({ key }: { key: string }) => key),
+		fallbackLng: LOCALE_KEYS[0].key,
+		debug: !import.meta.env.PROD,
+	});
+
+export default i18n;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import "./i18n";
 import { ColorModeProvider } from "@/components/ui/color-mode";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -7,6 +8,7 @@ import ReactDOM from "react-dom/client";
 import { OpenAPI } from "./client";
 import { routeTree } from "./routeTree.gen";
 import { system } from "./theme";
+
 OpenAPI.BASE = import.meta.env.VITE_API_URL;
 OpenAPI.TOKEN = async () => {
 	return localStorage.getItem("access_token") || "";
