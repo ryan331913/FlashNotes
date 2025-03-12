b4a311c0ecf4b332656f9b91c1fba175bd2b466d
ZorroGuardaPavos <4ndres.or@gmail.com>
2025-03-12 15:19:18 +0100

    fix: phone view of index

--- frontend/src/routes/_publicLayout/index.tsx
+++ frontend/src/routes/_publicLayout/index.tsx
@@ -1,7 +1,6 @@
 import { BlueButton, DefaultButton } from "@/components/commonUI/Button";
 import { Footer } from "@/components/commonUI/Footer";
 import {
-	Center,
 	Container,
 	Heading,
 	Image,
@@ -22,105 +21,105 @@ function LandingPage() {
 	const isAuthenticated = isLoggedIn();
 
 	return (
-		<Center minH="100%">
-			<Container maxW="container.xl" py={20}>
-				<Stack
-					direction={{ base: "column", lg: "row" }}
-					gap={10}
-					align="center"
-					justify="center"
-				>
-					<VStack gap={8} align={{ base: "center", lg: "flex-start" }} flex={1}>
-						<Image src="/favicon.svg" alt="FlashNotes favicon" w="100px" />
-						<Heading
-							as="h1"
-							size="2xl"
-							fontWeight="bold"
-							textAlign={{ base: "center", lg: "left" }}
-						>
-							{t("routes.publicLayout.index.title")}
-						</Heading>
-						<Text
-							fontSize="xl"
-							color="gray.500"
-							textAlign={{ base: "center", lg: "left" }}
-						>
-							{t("routes.publicLayout.index.description")}
-						</Text>
-						<Stack direction="row" gap={4}>
-							{isAuthenticated ? (
-								<Link to="/collections">
-									<BlueButton size="lg">
-										{t("general.actions.letsStudy")}!
-									</BlueButton>
-								</Link>
-							) : (
-								<>
-									<Link to="/signup">
-										<DefaultButton size="lg">
-											{t("general.actions.getStarted")}
-										</DefaultButton>
-									</Link>
-									<Link to="/login">
-										<DefaultButton size="lg">
-											{t("general.actions.login")}
-										</DefaultButton>
-									</Link>
-								</>
-							)}
-						</Stack>
-					</VStack>
-
-					<VStack flex={1} gap={8}>
-						<video
-							src="/preview.mp4"
-							autoPlay
-							loop
-							muted
-							style={{
-								width: "100%",
-								maxWidth: "18rem",
-								borderRadius: "12px",
-								border: "1px solid #565158",
-								boxShadow:
-									" 0px 2px 4px color-mix(in srgb, black 64%, transparent), 0px 0px 1px inset color-mix(in srgb, #d4d4d8 30%, transparent)",
-							}}
-						/>
-					</VStack>
-				</Stack>
-
-				<VStack gap={16} mt={20}>
-					<Heading as="h2" size="xl" textAlign="center">
-						{t("general.words.features")}
-					</Heading>
-					<Stack
-						direction={{ base: "column", md: "row" }}
-						gap={8}
-						align="stretch"
+		<Container
+			py={{ base: 20, lg: 0 }}
+			minH={{ base: "auto", lg: "100dvh" }}
+			display="flex"
+			flexDirection="column"
+			justifyContent="center"
+		>
+			<Stack
+				direction={{ base: "column", lg: "row" }}
+				gap={10}
+				align="center"
+				justify={{ base: "flex-start", lg: "center" }}
+			>
+				<VStack gap={8} align={{ base: "center", lg: "flex-start" }} flex={1}>
+					<Image src="/favicon.svg" alt="FlashNotes favicon" w="100px" />
+					<Heading
+						as="h1"
+						size="2xl"
+						fontWeight="bold"
+						textAlign={{ base: "center", lg: "left" }}
 					>
-						<Feature
-							title={t("routes.publicLayout.index.simpleDesign")}
-							description={t(
-								"routes.publicLayout.index.simpleDesignDescription",
-							)}
-						/>
-						<Feature
-							title={t("routes.publicLayout.index.aiGeneration")}
-							description={t(
-								"routes.publicLayout.index.aiGenerationDescription",
-							)}
-						/>
-						<Feature
-							title={t("routes.publicLayout.index.responsiveDesign")}
-							description={t(
-								"routes.publicLayout.index.responsiveDesignDescription",
-							)}
-						/>
+						{t("routes.publicLayout.index.title")}
+					</Heading>
+					<Text
+						fontSize="xl"
+						color="gray.500"
+						textAlign={{ base: "center", lg: "left" }}
+					>
+						{t("routes.publicLayout.index.description")}
+					</Text>
+					<Stack direction="row" gap={4}>
+						{isAuthenticated ? (
+							<Link to="/collections">
+								<BlueButton size="lg">
+									{t("general.actions.letsStudy")}!
+								</BlueButton>
+							</Link>
+						) : (
+							<>
+								<Link to="/signup">
+									<DefaultButton size="lg">
+										{t("general.actions.getStarted")}
+									</DefaultButton>
+								</Link>
+								<Link to="/login">
+									<DefaultButton size="lg">
+										{t("general.actions.login")}
+									</DefaultButton>
+								</Link>
+							</>
+						)}
 					</Stack>
 				</VStack>
-				<Footer version="0.0.16" />
-			</Container>
-		</Center>
+
+				<VStack flex={1} gap={8}>
+					<video
+						src="/preview.mp4"
+						autoPlay
+						loop
+						muted
+						style={{
+							width: "100%",
+							maxWidth: "18rem",
+							borderRadius: "12px",
+							border: "1px solid #565158",
+							boxShadow:
+								" 0px 2px 4px color-mix(in srgb, black 64%, transparent), 0px 0px 1px inset color-mix(in srgb, #d4d4d8 30%, transparent)",
+						}}
+					/>
+				</VStack>
+			</Stack>
+
+			<VStack gap={16} mt={20}>
+				<Heading as="h2" size="xl" textAlign="center">
+					{t("general.words.features")}
+				</Heading>
+				<Stack
+					direction={{ base: "column", md: "row" }}
+					gap={8}
+					align="stretch"
+				>
+					<Feature
+						title={t("routes.publicLayout.index.simpleDesign")}
+						description={t("routes.publicLayout.index.simpleDesignDescription")}
+					/>
+					<Feature
+						title={t("routes.publicLayout.index.aiGeneration")}
+						description={t("routes.publicLayout.index.aiGenerationDescription")}
+					/>
+					<Feature
+						title={t("routes.publicLayout.index.responsiveDesign")}
+						description={t(
+							"routes.publicLayout.index.responsiveDesignDescription",
+						)}
+					/>
+				</Stack>
+			</VStack>
+			<Footer version="0.0.16" />
+		</Container>
 	);
 }
 
