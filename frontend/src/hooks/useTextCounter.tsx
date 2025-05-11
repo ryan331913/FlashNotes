b59446127c0002f7745cb28dc18b1904e3f3217e
ryan.li <ryan331913@gmail.com>
2025-05-11 22:11:42 +0800

    Add corresponding translation

--- frontend/src/hooks/useTextCounter.tsx
+++ frontend/src/hooks/useTextCounter.tsx
@@ -2,18 +2,22 @@ import { toaster } from '@/components/ui/toaster'
 import { MAX_CHARACTERS } from '@/utils/text'
 import type { Editor } from '@tiptap/react'
 import { useCallback, useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 
 function useTextCounter(editor: Editor | null, setTextLength: (length: number) => void) {
+  
+  const { t } = useTranslation()
+
   const handleTextChange = useCallback(
-    (currentEditor: Editor) => {
+    (currentEditor: Editor | null) => {
       if (currentEditor) {
         const currentText = currentEditor.getText()
         setTextLength(currentText.length)
         if (currentText.length > MAX_CHARACTERS) {
           currentEditor.chain().undo().run()
           toaster.create({
-            title: 'Characters limitation',
-            description: 'Exceed the maximum characters',
+            title: t('components.editorTextCounter.title'),
+            description: t('components.editorTextCounter.description'),
             type: 'info',
           })
         }
@@ -24,7 +28,7 @@ function useTextCounter(editor: Editor | null, setTextLength: (length: number) =
 
   useEffect(() => {
     if (editor) {
-      const handleUpdate = ({ editor: currentEditor }: { editor: Editor }) => {
+      const handleUpdate = ({ editor: currentEditor }: { editor: Editor | null }) => {
         handleTextChange(currentEditor)
       }
       editor.on('update', handleUpdate)
